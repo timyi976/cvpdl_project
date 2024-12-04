0238e01182b2bd0750a0fb4ee375db049c50b822
guanboo-yang <b09902064@ntu.edu.tw>
2024-12-04 17:47:50 +0800

    feat: download images

---
+++ download_images.py
@@ -0,0 +1,49 @@
+import os
+import requests
+import re
+from PIL import Image
+
+def parse_input_file(file_path):
+    pattern = re.compile(r"^(\S+)\s+(\S+)$")
+    parsed_data = []
+
+    with open(file_path, "r") as file:
+        for line in file:
+            match = pattern.match(line.strip())
+            if match:
+                folder, url = match.groups()
+                parsed_data.append((folder, url))
+    return parsed_data
+
+def download_images(image_urls, save_directory):
+    os.makedirs(save_directory, exist_ok=True)
+    for idx, (folder_path, url) in enumerate(image_urls):
+        print(f"Image {idx + 1}/{len(image_urls)}: ", end="")
+        if "jpg" not in url and "png" not in url:
+            print(f"Not a JPG or PNG file.")
+            continue
+        ext = "jpg" if "jpg" in url else "png"
+        try:
+            response = requests.get(url, stream=True)
+            response.raise_for_status()
+            # file_name = os.path.join(save_directory, f"image_{idx + 1}.jpg")
+            file_name = os.path.join(save_directory, f"{folder_path}.{ext}")
+
+            with open(file_name, "wb") as file:
+                for chunk in response.iter_content(1024):
+                    file.write(chunk)
+
+            with Image.open(file_name) as img:
+                width, height = img.size
+                print(f"{width}×{height}")
+
+        except requests.exceptions.RequestException:
+            print(f"Failed to download")
+        
+        except Exception:
+            print(f"Unknown error")
+
+if __name__ == "__main__":
+    image_urls = parse_input_file("laion-ocr-index-url.txt")
+    print(f"Total {len(image_urls)} Images")
+    download_images(image_urls, "images")
